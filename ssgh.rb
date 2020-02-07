4ffdf96dca6d7b2e7ae4284a9db498de427ae297
Takuhiro Muta <417.72ki@gmail.com>
2020-02-07 12:35:31 +0900

    Update version to 1.0.1

--- ssgh.rb
+++ ssgh.rb
@@ -1,15 +1,15 @@
 class Ssgh < Formula
 	desc "Deliver stars on your behalf"
 	homepage "https://github.com/417-72KI/SSGH"
-	version "1.0.0"
-	url "https://github.com/417-72KI/SSGH/releases/download/1.0.0/ssgh.zip"
-	sha256 "9e1e73fa71004fe4ec4c4f6b711af2c6e2f38d777a44f2268c326eb98881f77e"
+	version "1.0.1"
+	url "https://github.com/417-72KI/SSGH/releases/download/1.0.1/ssgh.zip"
+	sha256 "663c81c47b143a00b2a7cd86451017075d2192f3333b4dc8c9c9fa8bddadf108"
 
 	def install
 		bin.install 'ssgh'
 	end
 
-	test do
+  test do
     system "false"
   end
 end
