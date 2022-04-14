cdca644f5a21b24c2d97fa3fdb552bf5e1c6415e
Takuhiro Muta <417.72ki@gmail.com>
2022-04-14 10:01:59 +0900

    Update version to 1.1.0

--- ssgh.rb
+++ ssgh.rb
@@ -1,13 +1,22 @@
 class Ssgh < Formula
-	desc "Deliver stars on your behalf"
-	homepage "https://github.com/417-72KI/SSGH"
-	version "1.0.4"
-	url "https://github.com/417-72KI/SSGH/releases/download/1.0.4/ssgh.zip"
-	sha256 "eb28d1a9795219ae0835b4b166fa27eb3b226fadb3056052f9e6b863f539ba2e"
+  desc "Deliver stars on your behalf"
+  homepage "https://github.com/417-72KI/SSGH"
+  version "1.1.0"
+  license "MIT"
+  head "https://github.com/417-72KI/SSGH.git", branch: "main"
 
-	def install
-		bin.install 'ssgh'
-	end
+  on_macos do
+      url "https://github.com/417-72KI/SSGH/releases/download/1.1.0/ssgh-macos-v1.1.0.zip"
+      sha256 "a95db7a9b1a9df3d861e51801ae486009711067c5d7d18812482962256ec7990"
+  end
+  on_linux do
+      url "https://github.com/417-72KI/SSGH/releases/download/1.1.0/ssgh-linux-v1.1.0.zip"
+      sha256 "45cef2890d1ac5676725cd281039d3149fbd4c5ab88ca0b8fb604b2fdd12e595"
+  end
+
+  def install
+      bin.install 'ssgh'
+  end
 
   test do
     system "false"
