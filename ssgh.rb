03a3119d53eae3465041cbe9c1ed9ace09381f9d
Takuhiro Muta <417.72ki@gmail.com>
2023-09-18 12:31:18 +0900

    Update ssgh to 1.4.0

--- ssgh.rb
+++ ssgh.rb
@@ -1,16 +1,16 @@
 class Ssgh < Formula
   desc "Deliver stars on your behalf"
   homepage "https://github.com/417-72KI/SSGH"
-  version "1.3.0"
+  version "1.4.0"
 
   on_macos do
     url "https://github.com/417-72KI/SSGH/archive/#{version}.tar.gz"
-    sha256 "6b981e96ac2cdbb96d4f977b971c576b7d715847b02fef2b0ac6ed15b15efe0b"
-    depends_on :xcode => ["13.3", :build]
+    sha256 "741eac3b9793630c5398a5adcdd87d016de8a698f8463b8ff074f5a5f0b4cc37"
+    depends_on :xcode => ["14.0", :build]
   end
   on_linux do
     url "https://github.com/417-72KI/SSGH/releases/download/#{version}/ssgh-linux-v#{version}.zip"
-    sha256 "b47f3fbfd57909c7a93cc4ccd7538f85f240e15cbe23a7df1cee1bccd59e5536"
+    sha256 "de7d60e28b4815dd7da69923c44746356b4c613d0a80f8288276842abd120293"
   end
   license "MIT"
   head "https://github.com/417-72KI/SSGH.git", branch: "main"
