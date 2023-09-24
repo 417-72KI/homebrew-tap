8413bfec3d03633b3c6c91c638c11edca25c879c
Takuhiro Muta <417.72ki@gmail.com>
2023-09-25 07:42:15 +0900

    Update r2acconverter to 0.3.0

--- r2acconverter.rb
+++ r2acconverter.rb
@@ -1,16 +1,16 @@
 class R2acconverter < Formula
   desc "Convert R.swift resources with Asset Catalog for Xcode 15"
   homepage "https://github.com/417-72KI/R2ACConverter"
-  version "0.2.0"
+  version "0.3.0"
 
   on_macos do
     url "https://github.com/417-72KI/R2ACConverter/archive/#{version}.tar.gz"
-    sha256 "7cd82df862ae1a0ddd8b7372aea0d37dc7c5741323518252faad3b3bc7f74345"
+    sha256 "ca43047cce4de946c5455ee79d6059374265988822932d433fb95104cde08fa4"
     depends_on :xcode => ["15.0", :build]
   end
   on_linux do
     url "https://github.com/417-72KI/R2ACConverter/releases/download/#{version}/r2acconverter-linux-v#{version}.zip"
-    sha256 "c38ca3478ed4016daad43c42d6bdd485a77de4d01f3ab61ac5f673a614440284"
+    sha256 "58cb04a4374f052f8f892499ad97dd265745971b27150008b4e70bc686c97ee0"
   end
   license "MIT"
   head "https://github.com/417-72KI/R2ACConverter.git", branch: "main"
